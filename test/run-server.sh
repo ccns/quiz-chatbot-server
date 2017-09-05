2351375805c4bfe553591c3a5b0373541901dcbb
gholk <c34031328@mail.ncku.edu.tw>
2017-09-05 11:05:38 +0800

    add convinient test script

---
+++ test/run-server.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+quiz_server=localhost:3000
+
+node start.js &
+sleep 3s
+
+post_querystring() {
+    querystring="$1"
+    url="$2"
+    curl -d "$querystring" "$url"
+}
+
+post_json() {
+    json_file="$1"
+    url="$2"
+    curl -H 'Content-Type: application/json' -d @"$json_file" "$url"
+}
+
+post_json question-database.json $quiz_server/question-database.json
+
+post_querystring 'user=1000&nickname=gholk&platform=curl' $quiz_server/user.json
